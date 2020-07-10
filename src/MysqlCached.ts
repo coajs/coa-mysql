d4d27ed5d2184679db58a76177a31c9cc504018c
Aex <spryti@qq.com>
2020-07-11 01:02:39 +0800

    对外开放缓存获取和缓存删除方法

--- src/MysqlCached.ts
+++ src/MysqlCached.ts
@@ -9,44 +9,44 @@ export class MysqlCached<Scheme> extends MysqlNative<Scheme> {
 
   async insert (data: SafePartial<Scheme>, trx?: Transaction) {
     const id = await super.insert(data, trx)
-    await this.cacheDeleteWork([id], [data])
+    await this.deleteCache([id], [data])
     return id
   }
 
   async mInsert (dataList: SafePartial<Scheme>[], trx?: Transaction) {
     const ids = await super.mInsert(dataList, trx)
-    await this.cacheDeleteWork(ids, dataList)
+    await this.deleteCache(ids, dataList)
     return ids
   }
 
   async updateById (id: string, data: SafePartial<Scheme>, trx?: Transaction) {
-    const dataList = await this.cacheChangeDataList([id], data, trx)
+    const dataList = await this.getCacheChangedDataList([id], data, trx)
     const result = await super.updateById(id, data, trx)
     if (result)
-      await this.cacheDeleteWork([id], dataList)
+      await this.deleteCache([id], dataList)
     return result
   }
 
   async updateForQueryById (id: string, query: Query, data: SafePartial<Scheme>, trx?: Transaction) {
-    const dataList = await this.cacheChangeDataList([id], data, trx)
+    const dataList = await this.getCacheChangedDataList([id], data, trx)
     const result = await super.updateForQueryById(id, query, data, trx)
     if (result)
-      await this.cacheDeleteWork([id], dataList)
+      await this.deleteCache([id], dataList)
     return result
   }
 
   async upsertById (id: string, data: SafePartial<Scheme>, trx?: Transaction) {
-    const dataList = await this.cacheChangeDataList([id], data, trx)
+    const dataList = await this.getCacheChangedDataList([id], data, trx)
     const result = await super.upsertById(id, data, trx)
-    await this.cacheDeleteWork([id], dataList)
+    await this.deleteCache([id], dataList)
     return result
   }
 
   async deleteByIds (ids: string[], trx?: Transaction) {
-    const dataList = await this.cacheChangeDataList(ids, undefined, trx)
+    const dataList = await this.getCacheChangedDataList(ids, undefined, trx)
     const result = await super.deleteByIds(ids, trx)
     if (result)
-      await this.cacheDeleteWork(ids, dataList)
+      await this.deleteCache(ids, dataList)
     return result
   }
 
@@ -55,45 +55,45 @@ export class MysqlCached<Scheme> extends MysqlNative<Scheme> {
   }
 
   async getById (id: string, pick = this.columns, trx?: Transaction, ms = this.ms) {
-    const result = await cache.warp(this.cacheNsp('id'), id, () => super.getById(id, this.columns, trx), ms)
+    const result = await cache.warp(this.getCacheNsp('id'), id, () => super.getById(id, this.columns, trx), ms)
     return this.pickResult(result, pick)
   }
 
   async getIdBy (field: string, value: string | number, trx?: Transaction) {
-    return await cache.warp(this.cacheNsp('index', field), '' + value, () => super.getIdBy(field, value, trx))
+    return await cache.warp(this.getCacheNsp('index', field), '' + value, () => super.getIdBy(field, value, trx))
   }
 
   async mGetByIds (ids: string[], pick = this.pick, trx?: Transaction, ms = this.ms) {
-    const result = await cache.mWarp(this.cacheNsp('id'), ids, ids => super.mGetByIds(ids, this.columns, trx), ms)
+    const result = await cache.mWarp(this.getCacheNsp('id'), ids, ids => super.mGetByIds(ids, this.columns, trx), ms)
     _.forEach(result, (v, k) => result[k] = this.pickResult(v, pick))
     return result
   }
 
   async truncate (trx?: Transaction) {
     await super.truncate(trx)
-    await this.cacheDeleteWork([], [])
+    await this.deleteCache([], [])
   }
 
   protected async findListCount (finger: Dic<any>[], query: Query, trx?: Transaction) {
-    const cacheNsp = this.cacheNsp('data')
+    const cacheNsp = this.getCacheNsp('data')
     const cacheId = 'list-count:' + secure.sha1($.sortQueryString(...finger))
     return await cache.warp(cacheNsp, cacheId, () => super.selectListCount(query, trx))
   }
 
   protected async findIdList (finger: Dic<any>[], query: Query, trx?: Transaction) {
-    const cacheNsp = this.cacheNsp('data')
+    const cacheNsp = this.getCacheNsp('data')
     const cacheId = 'list:' + secure.sha1($.sortQueryString(...finger))
     return await cache.warp(cacheNsp, cacheId, () => super.selectIdList(query, trx))
   }
 
   protected async findIdPageList (finger: Dic<any>[], page: Page, query: Query, trx?: Transaction) {
-    const cacheNsp = this.cacheNsp('data')
+    const cacheNsp = this.getCacheNsp('data')
     const cacheId = `page:${page.rows}:${page.last}:` + secure.sha1($.sortQueryString(...finger))
     return await cache.warp(cacheNsp, cacheId, () => super.selectIdPageList(page, query, trx))
   }
 
   protected async mGetCountBy (field: string, ids: string[], trx?: Transaction) {
-    const cacheNsp = this.cacheNsp('count', field)
+    const cacheNsp = this.getCacheNsp('count', field)
     return await cache.mWarp(cacheNsp, ids, async ids => {
       const rows = await this.table(trx).select({ id: field }).count({ count: this.key }).whereIn(field, ids).groupBy(field) as any[]
       const result = {} as Dic<number>
@@ -103,7 +103,7 @@ export class MysqlCached<Scheme> extends MysqlNative<Scheme> {
   }
 
   protected async getCountBy (field: string, value: string, query?: Query, trx?: Transaction) {
-    const cacheNsp = this.cacheNsp('count', field)
+    const cacheNsp = this.getCacheNsp('count', field)
     return await cache.warp(cacheNsp, value, async () => {
       const qb = this.table(trx).count({ count: this.key })
       query ? query(qb) : qb.where(field, value)
@@ -117,11 +117,11 @@ export class MysqlCached<Scheme> extends MysqlNative<Scheme> {
     return _.pick(data, pick) as T
   }
 
-  protected cacheNsp (...nsp: string[]) {
+  protected getCacheNsp (...nsp: string[]) {
     return this.system + ':' + this.name + ':' + nsp.join(':')
   }
 
-  private async cacheChangeDataList (ids: string[], data?: SafePartial<Scheme>, trx?: Transaction) {
+  protected async getCacheChangedDataList (ids: string[], data?: SafePartial<Scheme>, trx?: Transaction) {
     let has = true
     const resultList = [] as SafePartial<Scheme>[]
     if (data) {
@@ -135,10 +135,10 @@ export class MysqlCached<Scheme> extends MysqlNative<Scheme> {
     return resultList
   }
 
-  private async cacheDeleteWork (ids: string[], dataList: SafePartial<Scheme>[]) {
+  protected async deleteCache (ids: string[], dataList: SafePartial<Scheme>[]) {
     const deleteIds = [] as CacheDelete[]
-    deleteIds.push([this.cacheNsp('id'), ids])
-    deleteIds.push([this.cacheNsp('data'), []])
+    deleteIds.push([this.getCacheNsp('id'), ids])
+    deleteIds.push([this.getCacheNsp('data'), []])
     _.forEach(this.caches, (items, name) => {
       // name可能为index,count,或自定义
       items.forEach(item => {
@@ -149,7 +149,7 @@ export class MysqlCached<Scheme> extends MysqlNative<Scheme> {
           data && data[key] && ids.push(data[key])
         })
         ids.push(...keys.slice(1))
-        ids.length && deleteIds.push([this.cacheNsp(name, key), ids])
+        ids.length && deleteIds.push([this.getCacheNsp(name, key), ids])
       })
     })
     await cache.mDelete(deleteIds)
