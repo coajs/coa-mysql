40f42952915726adac2d1156bccb7476fd629e60
SongWuKong <syfzxx@vip.qq.com>
2022-07-16 19:03:11 +0800

    feat: add mysql config pool parameter (#16)

--- src/libs/MysqlBin.ts
+++ src/libs/MysqlBin.ts
@@ -10,11 +10,11 @@ export class MysqlBin {
   constructor(config: CoaMysql.Config) {
     // 创建数据库连接
     const mysql_env_main = config.databases.main || CoaError.throw('MysqlBin.ConfigMissing', '缺少主数据库配置')
-    const { host, port, user, password, charset, debug } = config
+    const { host, port, user, password, charset, debug, pool } = config
     const database = mysql_env_main.database
 
     // 数据库连接
-    const io = Knex({ client: 'mysql', connection: { host, port, user, password, database, charset, debug } })
+    const io = Knex({ client: 'mysql', connection: { host, port, user, password, database, charset, debug }, pool })
 
     io.on('query-error', (error: any) => {
       echo.error(error)
